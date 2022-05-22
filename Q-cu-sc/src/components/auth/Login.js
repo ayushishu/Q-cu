397fe5060bc5dd56c44a7381cab7056625c1dd29
Ayush <ayushayush.sadwal@gmail.com>
2022-05-22 20:51:02 +0530

    add

---
+++ Q-cu-sc/src/components/auth/Login.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import "./Login.css";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import { auth, provider } from "../../firebase";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((e) => {
+      alert(e.message);
+    });
+  };
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+      })
+      .catch((e) => alert(e.message));
+  };
+
+  const registerSignIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        if (auth) {
+          console.log(auth);
+        }
+      })
+      .catch((e) => alert(e.message));
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <div className="login__logo">
+          <img
+            // src="https://www.logomaker.com/api/main/images/1j+ojlxEOMkX9Wyqfhe43D6khv6FqR5OmxzIwWJqZ1FS6BQf9TZxhP8soOZqaEgbuQRJ1g9ZLYQmwXs+R8VL1VsgqjKMP45AW3h8yC4A"
+            alt=""
+          />
+        </div>
+        <div className="login__desc">
+          <p>Q&A platform that empowers students to share and grow their knowledge</p>
+          <p style={{ color: "royalblue", fontSize: "25px" }}>
+            {/* HandCrafted with ❤️ by{" "} */}
+          </p>
+          {/* <h3>Code With Akky</h3> */}
+        </div>
+        <div className="login__auth">
+          <div className="login__authOptions">
+            <div className="login__authOption">
+              <img
+                className="login__googleAuth"
+                src="https://media-public.canva.com/MADnBiAubGA/3/screen.svg"
+                alt=""
+              />
+              <p onClick={signIn}>Continue With Google</p>
+            </div>
+            <div className="login__authOption">
+              <img
+                className="login__googleAuth"
+                src="https://1000logos.net/wp-content/uploads/2016/11/Facebook-logo-500x350.png"
+                alt=""
+              />
+              <span>Continue With Facebook</span>
+            </div>
+            <div className="login__authDesc">
+              <p>
+                <span style={{ color: "blue", cursor: "pointer" }}>
+                  Sign Up With Email
+                </span>
+                . By continuing you indicate that you have read and agree to
+                Q-Cu
+                <span style={{ color: "blue", cursor: "pointer" }}>
+                  Terms of Service{" "}
+                </span>
+                and{" "}
+                <span style={{ color: "blue", cursor: "pointer" }}>
+                  Privacy Policy
+                </span>
+                .
+              </p>
+            </div>
+          </div>
+          <div className="login__emailPass">
+            <div className="login__label">
+              <h4>Login</h4>
+            </div>
+            <div className="login__inputFields">
+              <div className="login__inputField">
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="login__inputField">
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="login__forgButt">
+              <small>Forgot Password?</small>
+              <button onClick={handleSignIn}>Login</button>
+            </div>
+            <button onClick={registerSignIn}>Register</button>
+          </div>
+        </div>
+        <div className="login__lang">
+          {/* <p>हिन्दी</p> */}
+          {/* <ArrowForwardIosIcon fontSize="small" /> */}
+        </div>
+        <div className="login__footer">
+          {/* <p>About</p>
+          <p>Languages</p>
+          <p>Careers</p>
+          <p>Businesses</p>
+          <p>Privacy</p>
+          <p>Terms</p>
+          <p>Contact</p>
+          <p>&copy; Quora Fake Inc. 2021</p> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
